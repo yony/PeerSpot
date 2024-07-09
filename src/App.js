faa93549ceb39a29e6077faf131aa4be34c585e6
Yony Limor <yonilimor@PEERSPOT-056.local>
2024-07-09 16:38:01 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,166 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useCallback, useState } from 'react';
+import SideNavMenu from './components/SideNavMenu.js';
+import SearchResults from './components/SearchResults.js';
+import { Avatar, Badge, Breadcrumb, Button, ConfigProvider, Input } from 'antd';
+import { ArrowLeftOutlined, HomeOutlined, MenuFoldOutlined } from '@ant-design/icons';
+
+
+const { Search } = Input;
+const onSearch = (value, _e, info) => console.log(info?.source, value);
+const resultSections = [
+  {
+    title: "Recent Content Type",
+    showMoreButton: false,
+  },
+  {
+    title: "Product",
+    showMoreButton: true,
+  },
+  {
+    title: "Comparison",
+    showMoreButton: true,
+  },
+  {
+    title: "Category",
+    showMoreButton: true,
+  },
+]
 
 function App() {
+
+  const [menuLevel, setMenuLevel] = useState(0);
+
+  const styleTransition = {
+    transform: `translate3d(${-menuLevel * 300}px, 0, 0)`,
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider
+      theme={{
+        token: {
+          // colorPrimary: '#FFD700',
+          colorTextLightSolid: "#000000",
+          borderRadius: 0,
+        },
+        components: {
+          Button: {
+            borderRadius: '2px',
+            colorPrimary: '#FFD700',
+            colorPrimaryActive: '#E0BD00',
+            colorPrimaryHover: '#F7C602',
+          },
+        },
+      }}
+    >
+      <div className="ps-app">
+        <aside className="ps-side-bar">
+          <header className="ps-side-bar__header">
+            <Button type="text" shape="circle" icon={<MenuFoldOutlined />}></Button>
+            <img src={logo} className="ps-logo" alt="logo" />
+            <div style={{ width: '40px'}}></div>
+          </header>
+          <SideNavMenu />
+        </aside>
+        <section className="ps-main">
+          <header className="ps-header">
+            <Breadcrumb 
+              items={[
+                {
+                  href: '',
+                  title: <HomeOutlined />,
+                },
+                {
+                  href: '',
+                  title: 'PS Modules',
+                },
+                {
+                  href: '',
+                  title: 'Content Types',
+                },
+                {
+                  href: '',
+                  title: 'Steps',
+                },
+                {
+                  title: 'Prompts',
+                },
+              ]}
+            />
+            <Badge dot>
+              <Avatar
+                style={{
+                  backgroundColor: '#FFF8E8',
+                  border: '1px solid #E5E6E1',
+                  color: '#A97F13',
+                  }}>YL</Avatar>
+            </Badge>
+          </header>
+          <div className="ps-body">
+            <aside className='ps-items-panel'>
+              <div className='ps-items-panel__container'
+                   style={styleTransition}>
+                <div className='ps-items-panel__level ps-items-panel__level--main'>                  
+                  <div className='ps-items-panel__search'>
+                    <Search
+                      className='ps-search'
+                      placeholder="Search content type..."
+                      onSearch={onSearch}
+                      style={{
+                        width: '100%',
+                      }}
+                    />
+                  </div>
+                  <div className='ps-search-results'>
+                    {resultSections.map((section, index) => (  
+                      <SearchResults
+                        childMenuLevel={1}
+                        onChange={setMenuLevel}
+                        showMoreButton={section.showMoreButton}
+                        title={section.title}
+                      />
+                    ))}
+                  </div>
+                  <div className='ps-items-panel__cta'>
+                    <Button type="primary" onClick={() => setMenuLevel(1)}>New Content Type</Button>
+                  </div>
+                </div>
+                <div className='ps-items-panel__level ps-items-panel__level--child'>
+                  <header className="ps-items-panel__header">
+                    <Button type="text" shape="circle" icon={<ArrowLeftOutlined />} onClick={() => setMenuLevel(0)}></Button>
+                    <div>Steps</div>
+                    <div style={{ width: '40px'}}></div>
+                  </header>
+                  <div className='ps-search-results'>
+                    Steps 123456
+                  </div>
+                  <div className='ps-items-panel__cta'>
+                    <Button type="primary" onClick={() => setMenuLevel(2)}>New Step</Button>
+                  </div>
+                </div>
+                <div className='ps-items-panel__level ps-items-panel__level--child'>
+                  <header className="ps-items-panel__header">
+                    <Button type="text" shape="circle" icon={<ArrowLeftOutlined />} onClick={() => setMenuLevel(1)}></Button>
+                    <div>Prompts</div>
+                    <div style={{ width: '40px'}}></div>
+                  </header>
+                  <div className='ps-search-results'>
+                    Prompts 123456
+                  </div>
+                  <div className='ps-items-panel__cta'>
+                    <Button type="primary">New Prompt</Button>
+                  </div>
+                </div>
+              </div>
+            </aside>
+            <div className='ps-item-container'>
+              Form
+            </div>
+          </div>
+        </section>
+      </div>
+    </ConfigProvider>
   );
 }
 
---
+++ src/components/SearchResults.js
@@ -0,0 +1,94 @@
+import './SearchResults.css';
+import React, { useEffect, useState } from 'react';
+import { List, Skeleton} from 'antd';
+
+const count = 5;
+const fakeDataUrl = `https://randomuser.me/api/?results=${count}&inc=name,gender,email,nat,picture&noinfo`;
+
+function SearchResults({childMenuLevel, onChange, showMoreButton = false, title }) {
+    const [initLoading, setInitLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState([]);
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+        async function fetchData(){
+            try {
+                await fetch(fakeDataUrl)
+                    .then((res) => res.json())
+                    .then((res) => {
+                        setInitLoading(false);
+                        setData(res.results);
+                        setList(res.results);
+                    });
+            } catch (err) {
+                console.error('--- Error loading fake data ---', err);
+            }
+        }
+        fetchData();
+    }, []);
+
+    const onLoadMore = () => {
+        setLoading(true);
+        setList(
+            data.concat(
+                [...new Array(count)].map(() => ({
+                loading: true,
+                name: {},
+                picture: {},
+                })),
+            ),
+        );
+        fetch(fakeDataUrl)
+            .then((res) => res.json())
+            .then((res) => {
+                const newData = data.concat(res.results);
+                setData(newData);
+                setList(newData);
+                setLoading(false);
+                // Resetting window's offsetTop so as to display react-virtualized demo underfloor.
+                // In real scene, you can using public method of react-virtualized:
+                // https://stackoverflow.com/questions/46700726/how-to-use-public-method-updateposition-of-react-virtualized
+                window.dispatchEvent(new Event('resize'));
+            });
+    };
+
+    const loadMore =
+        !initLoading && !loading && showMoreButton ? (
+            // eslint-disable-next-line jsx-a11y/anchor-is-valid
+            <a className='ps-search-results__section__more'
+               onClick={onLoadMore}>More</a>
+        ) : null;
+
+    const itemClicked = (item) => {
+        // step into the child element
+        console.log('Item clicked', item.name?.first);
+        console.log('Level clicked', childMenuLevel);
+
+        onChange(childMenuLevel);
+    }
+
+    return (
+        <List
+            size="small"
+            header={<div>{title}</div>}
+            className="ps-search-results__section"
+            loading={initLoading}
+            loadMore={loadMore}
+            dataSource={list}
+            renderItem={(item) => (
+                <List.Item onClick={() => itemClicked(item)}>
+                    <Skeleton title={false} loading={item.loading} active>
+                        <List.Item.Meta
+                            // title={<a href="https://ant.design">{item.name?.last}</a>}
+                            // eslint-disable-next-line jsx-a11y/anchor-is-valid
+                            description={<a>{`${item.name?.last} ${item.name?.first}`}</a>}
+                        />
+                    </Skeleton>
+                </List.Item>
+            )}
+        />
+    )
+}
+
+export default SearchResults;
