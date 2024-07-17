9a4c1cc422165bc44e212db685b41eaf2283b389
Yony Limor <yonilimor@PEERSPOT-056.local>
2024-07-17 20:09:05 +0300

    added feedback message for copy to clipboard

--- src/components/CopyToClipboardItem.js
+++ src/components/CopyToClipboardItem.js
@@ -1,22 +1,34 @@
 import './CopyToClipboardItem.css';
 import React from 'react';
-import { Button } from 'antd';
+import { Button, message } from 'antd';
 import { CopyOutlined } from '@ant-design/icons';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 
 function CopyToClipboardItem({text}) {
+    const [messageApi, contextHolder] = message.useMessage();
+    
+    const copiedToClipboard = () => {
+        messageApi.open({
+        type: 'success',
+        content: 'Successfully copied to clipboard',
+        });
+    };
+
     return (
-        <CopyToClipboard
-            classname='ps-context__copy'
-            text={text}
-            // onCopy={ copiedToClipboard }
-        >
-            <div className='ps-context'>
-                <span className='ps-context__text'>{text}</span>
+        <>
+            { contextHolder }
+            <CopyToClipboard
+                classname='ps-context__copy'
+                text={text}
+                onCopy={ copiedToClipboard }
+            >
+                <div className='ps-context'>
+                    <span className='ps-context__text'>{text}</span>
                     <Button className='ps-context__button' icon={<CopyOutlined />} />
-            </div>
-        </CopyToClipboard>
+                </div>
+            </CopyToClipboard>
+        </>
     )
 }
 
